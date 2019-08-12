6026f7a33f5b80adddd4908e0f1630935798b497
karkisunil1200 <karkisunil1200@gmail.com>
2019-08-13 00:09:55 +0545

    Initial commit

---
+++ src/main/java/com/lambda/countries/CountryList.java
@@ -0,0 +1,15 @@
+package com.lambda.countries;
+
+public class CountryList {
+
+    private String name;
+    private long population;
+    private long landMass;
+    private long medianAge;
+
+    public CountryList(String name, long population, long landMass, long medianAge) {
+
+    }
+}
+
+//a country name, population, land mass size, and median age
